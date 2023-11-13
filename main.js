753e03fbd5a5e684f22c47eab25bc643d27c8453
LUCA-DesComBes <ldescombes@edenschool.fr>
2023-11-13 15:12:37 +0100

    change js

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-let chekbox = document.querySelector('.switch')
-function checkedCheckbox(n) {
-    
-}
+let CSB = document.querySelectorAll(input)
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  for(i = 0; i<CSB.length; i++){
+    CSB.addEventListener("click", getRandomInt)
+    let r = document.getRandomInt(min,max)
+  }
+  CSB[r]
